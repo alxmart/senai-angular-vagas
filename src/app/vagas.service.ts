f5c8d058cfb4ef6b21734177765bc04f24c57d16
alxmart <alxmartinez@gmail.com>
2022-02-24 16:49:19 -0300

    Criando modelo e serviço

--- src/app/vagas.service.ts
+++ src/app/vagas.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VagasService {
 
-  constructor() { }
+  private url = "";
+
+  constructor( private _httpClient: HttpClient) { }
+
+  getVagas(): Observable<Vaga[]> {
+
+    return this._httpClient.get<Vaga[]>(this.url);
+
+
+  }
+
 }
