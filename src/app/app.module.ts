6cdc0489f686c8268196096f143d74ec1e5d6646
alxmart <alxmartinez@gmail.com>
2022-02-25 16:48:09 -0300

    Ajustes para criacao das rotas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,25 @@ import { AppComponent } from './app.component';
 import { TesteluizComponent } from './testeluiz/testeluiz.component';
 import { Teste2Component } from './teste2/teste2.component';
 import { MuralVagasComponent } from './mural-vagas/mural-vagas.component';
+import { RouterModule } from '@angular/router';
+import { PainelVagasComponent } from './painel-vagas/painel-vagas.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MuralVagasComponent
+    MuralVagasComponent,
+    PainelVagasComponent,
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule  
+    HttpClientModule,
+    RouterModule,
+    AppRoutingModule,  
   ],
   providers: [],
   bootstrap: [AppComponent]
